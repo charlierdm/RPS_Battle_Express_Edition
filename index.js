d195afe43ccfe3a710a6c71d459ad445bceb042b
charlierdm <charlie.davismiller@gmail.com>
2021-05-25 20:28:37 +0100

    post request function, data accessed in seperate view + ejs added

--- index.js
+++ index.js
@@ -3,12 +3,23 @@ const app = express();
 const port = 3000;
 
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.sendFile('index')
+  res.render('./index')
 })
 
-app.get('/choose_rps', (req, res) => {
+app.post('/', (req, res) => {
+  console.log(req.body)
+  res.render('./choose_rps', {data: req.body})
+})
+
+app.get('./choose_rps', (req, res) => {
+  res.sendFile('./choose_rps')
+})
+
+app.post('./choose_rps', (req, res) => {
   res.sendFile('./choose_rps')
 })
 
