f68e5f15ae709683658baac05c88683321e07df1
charlierdm <charlie.davismiller@gmail.com>
2021-05-25 17:55:46 +0100

    multiple routes set up in public folder

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express');
-const path = require('path');
 const app = express();
 const port = 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.sendFile('index')
+})
+
+app.get('/choose_rps', (req, res) => {
+  res.sendFile('./choose_rps')
+})
 
 app.listen(port, () => {
   console.log(`RPS app listening at port ${port}`)
