5d5db39aadfac932446f76eb54088dced9fd7812
Charlie <charlie.davismiller@gmail.com>
2021-06-01 22:35:07 +0100

    user name function created and rps choice will not submit unless a selection is made

--- public/js/interface.js
+++ public/js/interface.js
@@ -1,3 +1,11 @@
 const displayChoice = (value) => {
 	document.getElementById("selection").innerHTML = `You have selected ${value}, prepare for battle!`;
 }
+
+const emptyName = () => {
+	let userInput = document.getElementById("enter-name").value;
+	if(userInput === '') {
+		alert('Please enter a name.');
+		return false;
+	}
+}
