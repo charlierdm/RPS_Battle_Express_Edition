55c17b188b9a7bbe20c2e41e14e3dae21643679c
Charlie <charlie.davismiller@gmail.com>
2021-06-11 14:17:01 +0100

    all JS refactored. code formatted by prettier

--- index.js
+++ index.js
@@ -8,25 +8,23 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
-app
-  .route("/")
-  .get((req, res) => {
-    res.render("./index");
-  });
+app.route("/").get((req, res) => {
+  res.render("./index");
+});
 
-app
-  .route("/choose_rps")
-  .post((req, res) => {
-    res.render("./choose_rps", { data: req.body });
-  });
+app.route("/choose_rps").post((req, res) => {
+  res.render("./choose_rps", { data: req.body });
+});
 
-  app
-    .route("/results")
-    .post((req, res) => {
-      const computerChoice = new ComputerPlayer().play();
-      const game = new Game(req.body.user_choice, computerChoice).results();
-      res.render("./results", { data: req.body, results: game, computerChoice: computerChoice });
-    });
+app.route("/results").post((req, res) => {
+  const computerChoice = new ComputerPlayer().play();
+  const game = new Game(req.body.user_choice, computerChoice).results();
+  res.render("./results", {
+    data: req.body,
+    results: game,
+    computerChoice: computerChoice,
+  });
+});
 
 app.listen(port, () => {
   console.log(`RPS app listening at port ${port}`);
--- public/js/interface.js
+++ public/js/interface.js
@@ -1,11 +1,31 @@
 const displayChoice = (value) => {
-	document.getElementById("selection").innerHTML = `<br>You have selected ${value}, prepare for battle!`;
-}
+  document.getElementById(
+    "selection"
+  ).innerHTML = `<br>You have selected ${value}, prepare for battle!`;
+};
 
 const emptyName = () => {
-	let userInput = document.getElementById("enter-name").value;
-	if(userInput === '') {
-		alert('Please enter a name.');
-		return false;
-	}
-}
+  let userInput = document.getElementById("enter-name").value;
+  if (userInput === "") {
+    alert("Please enter a name.");
+  }
+};
+
+document.getElementById("button1").addEventListener("click", (event) => {
+  emptyName();
+  event.preventDefault();
+})
+
+document.getElementById("svg1").addEventListener("click", () => {
+  displayChoice('rock');
+});
+
+document.getElementById("svg2").addEventListener("click", () => {
+  displayChoice("paper");
+});
+
+document.getElementById("svg3").addEventListener("click", () => {
+  displayChoice("scissors");
+});
+
+
